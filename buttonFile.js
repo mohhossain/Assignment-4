9a292da20b3b39d6f8303cde75e019c7d2aee4d4
Damir Kamalov <dkamalov8@gmail.com>
2021-01-07 14:10:35 -0500

    Create buttonFile.js

---
+++ buttonFile.js
@@ -0,0 +1,9 @@
+function buttonFunction(){
+
+    document.getElementById(textB).textContent = "I'm right!";
+}
+
+function realButtonFunction(){
+
+    document.getElementById(textB).textContent = "No I'm right!";
+}
